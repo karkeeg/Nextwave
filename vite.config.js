a5bd6f5e75e0305c4847e794ea2909d52eff9abf
karkeeg <karkibibek642@gmail.com>
2025-09-05 13:46:44 +0545

    Resolve

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    cssCodeSplit: true,
+    cssCodeSplit: true, // ensure CSS is split out
   },
 });
